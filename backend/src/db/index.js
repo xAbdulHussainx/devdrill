f7b1782165258618aa8c12d90c8233c8095b97fc
Abdul Hussain <abdulhussain675@gmail.com>
2026-05-17 22:21:27 -0400

    fix ssl for railway postgres

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -6,6 +6,7 @@ const { Pool } = pg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 export const initDB = async () => {
